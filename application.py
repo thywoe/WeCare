6319fa71691a088e8cbacd119df599bd3abf6c00
babalola taiwo <bamibabson@gmail.com>
2019-09-02 18:36:24 +0100

    ensure responses are not cached

--- application.py
+++ application.py
@@ -5,3 +5,13 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+#Ensure templates auto-reloads
+app.config["TEMPLATES_AUTO_RELOADED"] = True
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
