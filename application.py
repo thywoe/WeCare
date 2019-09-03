7d3eac935fe150c44c05de3bcf5d465696a27f63
babalola taiwo <bamibabson@gmail.com>
2019-09-03 18:30:12 +0100

    design login route

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from cs50 import SQL
 
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -24,6 +24,29 @@ app.config["SESSION_FILE_DIR"] = "filesystem"
 Session(app)
 
 db = SQL("sqlite:///wecare.db")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+
+    if request.method == "POST":
+        if not request.form.get("email"):
+            return apology("must provide username",403)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        rows = db.execute("SELECT * FROM users WHERE email = :email", email = request.form.get("email"))
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
 
 
 
