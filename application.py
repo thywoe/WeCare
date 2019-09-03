6126f25d20a8cb96387f0f971019ccc8367cf5dc
babalola taiwo <bamibabson@gmail.com>
2019-09-03 19:03:56 +0100

    design a helper file for client

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required
 
 app = Flask(__name__)
 
