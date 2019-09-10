daeab754c6c9a61c881d7127745bda1fe2acdd69
thywo babson <bamibabson@gmail.com>
2019-09-10 12:04:41 +0100

    add image route

--- static/scripts/jquery.js
+++ static/scripts/jquery.js
@@ -1,14 +1,34 @@
-$(".hosp-box").on("click",".hosp-wrapper", (e) => {
-    let target = e.target
-    if(target.className == "hosp-wrapper"){
-       let hosp_name = $(".hosp-h3").text()
-       $.ajax({
-        url: "/appointment?hosp_name="+hosp_name,
-        method: "GET",
-        success:function(data){
-            window.location = "/appointment?hosp_name="+hosp_name
-        }
+$(document).ready(function(){
+    $(".hosp-box").on("click",".hosp-wrapper", ({target}) => {
+        // let target = e.target
+        // if(target.className == "hosp-wrapper"){
+           let hosp_name = target.innerText
+           let name = hosp_name.split("\n")
+           console.log(name[0])
+           $.ajax({
+            url: "/appointment?hosp_name="+name[0],
+            method: "GET",
+            success:function(data){
+                window.location = "/appointment?hosp_name="+name[0]
+            }
+        })
+        })
+    
+    
+    $(".fas").on("click", (e) => {
+        e.stopPropagation()
+    
     })
-    }
 
-})
+    $(".place").on("click", (e) => {
+        e.stopPropagation()
+    
+    })
+
+    $(".add1").on("click", (e) => {
+        e.stopPropagation()
+    
+    })
+});
+
+
